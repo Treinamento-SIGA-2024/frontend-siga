b92757f419b5998aa21e6e0ff2a96ef35552c5d0
leaaaaaao <gabrielfleao0@gmail.com>
2024-01-22 21:30:19 -0300

    Botão de limpar os campos e redirect para a página anterior ao cancelar a criação

--- src/pages/secretaria/CreateEstagio.vue
+++ src/pages/secretaria/CreateEstagio.vue
@@ -1,53 +1,59 @@
 <template>
+
   <PageTitle title="NOVA PROPOSTA"/>
 
-  <p>Função:</p>
-  <v-text-field/>
+  <v-form id="form">
 
-  <p>Nome da Empresa:</p>
-  <v-text-field/>
+    <v-btn id="limpar" @click="resetForm">Limpar</v-btn>
 
-  <div class="camposRow">
-    <div class="filhosRow">
-      <p class="labelCampos">Remunerado? </p>
-      <v-checkbox-btn :color="this.global.colors.green3" v-model="remunerado"
-                      :label="remunerado ? 'Sim' : 'Não'"></v-checkbox-btn>
+    <p>Função:</p>
+    <v-text-field v-model="funcao"/>
+
+    <p>Nome da Empresa:</p>
+    <v-text-field v-model="empresa"/>
+
+    <div class="camposRow">
+      <div class="filhosRow">
+        <p class="labelCampos">Remunerado? </p>
+        <v-checkbox-btn :color="this.global.colors.green3" v-model="remunerado"
+                        :label="remunerado ? 'Sim' : 'Não'"></v-checkbox-btn>
+      </div>
+
+      <div class="filhosRow">
+        <p class="labelCampos">Valor da bolsa:</p>
+        <v-text-field prefix="R$" :disabled="!remunerado" v-model="bolsa"/>
+      </div>
     </div>
 
-    <div class="filhosRow">
-      <p class="labelCampos">Valor da bolsa:</p>
-      <v-text-field prefix="R$" :disabled="!remunerado"/>
+    <div class="camposRow">
+      <div class="filhosRow">
+        <p>Carga Horária:</p>
+        <v-text-field suffix="horas/sem" v-model="horas"/>
+      </div>
+
+      <div class="filhosRow">
+        <p>Vagas disponíveis:</p>
+        <v-text-field v-model="vagas"/>
+      </div>
     </div>
-  </div>
 
-  <div class="camposRow">
-    <div class="filhosRow">
-      <p>Carga Horária:</p>
-      <v-text-field suffix="horas/sem"/>
+    <p>Modalidade:</p>
+    <div id="selectDiv">
+      <v-select v-model="modalidade" variant="outlined"
+                class="select" label="Selecione" :items="itens"/>
     </div>
 
-    <div class="filhosRow">
-      <p>Vagas disponíveis:</p>
-      <v-text-field/>
+    <p>Descrição:</p>
+    <v-textarea v-model="descricao"/>
+
+    <div id="submit">
+      <ButtonCard id="salvar" title="Salvar"
+                  :style="{height: '50px'}"/>
+      <ButtonCard id="cancelar" title="Cancelar"
+                  :style="{height: '50px'}"
+                  @click="this.$router.push('/secretaria')"/>
     </div>
-  </div>
-
-  <p>Modalidade:</p>
-  <div id="selectDiv">
-    <v-select class="select" label="Selecione"
-              :items="['Remoto','Híbrido','Presencial']"/>
-  </div>
-
-  <p>Descrição:</p>
-  <v-textarea/>
-
-  <div id="submit">
-    <ButtonCard id="salvar" title="Salvar"
-                :style="{height: '50px'}"/>
-    <ButtonCard id="cancelar" title="Cancelar"
-                :style="{height: '50px'}"/>
-  </div>
-
+  </v-form>
 </template>
 
 <script>
@@ -61,9 +67,31 @@ export default {
   name: "SecretariaInicial",
   data() {
     return {
+      funcao: '',
+      empresa:'',
       remunerado: false,
+      bolsa:'',
+      horas:'',
+      vagas:'',
+      modalidade: null,
+      itens: ['Remoto','Híbrido','Presencial'],
+      descricao:'',
     }
   },
+
+  methods: {
+    resetForm(){
+      this.funcao='';
+      this.empresa='';
+      this.remunerado= false;
+      this.bolsa='';
+      this.horas='';
+      this.vagas='';
+      this.modalidade= null;
+      this.descricao='';
+    }
+  },
+
   components: {
     title: String,
     Header,
@@ -79,6 +107,21 @@ export default {
 .select {
   height: 20px;
   width: 500px;
+}
+
+#form {
+  display: flex;
+  flex-direction: column;
+}
+
+#limpar {
+  font-size: 12px;
+  width: 50px;
+  height: 30px;
+  color: white;
+  background-color: #adadad;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.21);
+  align-self: flex-end;
 }
 
 #salvar {
