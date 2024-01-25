ccf94045382f305c586a0bf47b4f816695c4f153
Tiago d'Almeida <dalmeidatiago@gmail.com>
2024-01-24 22:35:05 -0300

    PopUp de erro, de confirmação e validação do formulario

--- src/services/iniciacaoCientifica.js
+++ src/services/iniciacaoCientifica.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 const api_url = 'http://localhost:8080';
 
 export const getAllIcs = async () => {
-    const { data } = await axios.get(`${api_url}/ic`);
+    const { data } = await axios.get(`${api_url}/iniciacao_cientifica`);
 
     return data;
 };
 
 export const getIcById = async (icId) => {
-    const { data } = await axios.get(`${api_url}/ic/${icId}`);
+    const { data } = await axios.get(`${api_url}/iniciacao_cientifica/${icId}`);
 
     return data;
 };
