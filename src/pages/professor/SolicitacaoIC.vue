644e7fb3c542f1683b84c30ca2526d8a962c7c66
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2024-01-25 00:15:00 -0300

    consertando imports em professor ver pedidos de aluno ic

--- src/pages/professor/SolicitacaoIC.vue
+++ src/pages/professor/SolicitacaoIC.vue
@@ -7,7 +7,7 @@
   <v-container v-if="this.inscricoes.length">
     <v-row v-for="(inscricao, i) in inscricoes" :key="i" cols="auto">
       <v-col>
-        <CardSolicitacaoIC :inscricao="inscricao" @refreshSituacao = "getInscricoesIc"/>
+        <CardSolicitacaoIC :inscricao="inscricao" :iniciacaoCientifica="iniciacaoCientifica" @refreshSituacao = "getInscricoesIc"/>
       </v-col>
     </v-row>
   </v-container>
@@ -23,6 +23,7 @@ import CardSolicitacaoIC from "@/components/CardSolicitacaoIC.vue";
 import {getAllIcPendentes} from "@/services/professorService.js";
 import Loading from "@/components/Loading.vue";
 import PopUp from "@/components/PopUp.vue";
+import {getIcById} from "@/services/iniciacaoCientifica.js";
 
 export default {
   components: {PopUp, Loading, CardSolicitacaoIC, DadosPessoais},
@@ -31,6 +32,7 @@ export default {
       professorMatricula: "200000001",
       icId: "1",
       inscricoes:[],
+      iniciacaoCientifica:{},
       metodos:{
         msg:"Recarregar página?",
         aceitarAction:this.getInscricoesIc,
@@ -41,8 +43,11 @@ export default {
     async getInscricoesIc() {
       console.log("ooooi")
       const inscricoes = await getAllIcPendentes(this.professorMatricula, this.icId);
+      const ic = await getIcById(this.icId);
 
       console.log(inscricoes)
+      console.log(ic)
+      this.iniciacaoCientifica = ic;
       this.inscricoes = inscricoes;
       console.log(this.inscricoes.length)
     }
