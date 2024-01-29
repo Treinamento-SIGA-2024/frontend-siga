2af3cdfd1225c5f850963e0b57f983c7b415cad8
leaaaaaao <gabrielfleao0@gmail.com>
2024-01-29 19:24:44 -0300

    Filtros de estágio

--- src/pages/aluno/ListEstagio.vue
+++ src/pages/aluno/ListEstagio.vue
@@ -2,9 +2,53 @@
   <PageTitle title="PROPOSTAS DE ESTÁGIO" />
   <PopUpErro :erro="erro"/>
   <Loading v-if="estagios.length === 0 && !erro"/>
+
+  <v-btn v-if="!filtros" id="addFiltro" @click="filtros = true">
+      <PlusIcon />
+      <p>Adicionar filtros</p>
+    </v-btn>
+    
+    <v-container v-if="filtros" class="filter-container">
+      
+      <CloseIcon @Click="fechaFiltros" id="closeFiltros" alt="Cancelar filtros"/>
+
+      <div class="selects-container">
+        
+        <h3 style="margin-bottom:20px" >Aplique os filtros desejados</h3>
+
+        <v-autocomplete 
+          clearable
+          label="Selecione as empresas"
+          :items="this.empresas"
+          multiple
+          variant="outlined"
+          v-model="empresasSelecionadas"
+        ></v-autocomplete>
+
+        <v-autocomplete
+          clearable
+          label="Selecione os cargos"
+          :items="this.cargos"
+          multiple
+          variant="outlined"
+          v-model="cargosSelecionados"
+        ></v-autocomplete>
+
+        <v-autocomplete
+          clearable
+          label="Selecione as modalidades"
+          :items="this.modalidades"
+          multiple
+          variant="outlined"
+          v-model="modalidadesSelecionadas"
+        ></v-autocomplete>
+      </div>
+      
+    </v-container>
+
   <v-container>
-    <v-row align="center" justify="center" v-if="estagios.length !== 0 && !erro">
-      <v-col v-for="(estagio) in estagios" :key="estagio.id" cols="auto">
+    <v-row align="center" justify="center" v-if="estagiosFiltrados.length !== 0 && !erro">
+      <v-col v-for="(estagio) in estagiosFiltrados" :key="estagio.id" cols="auto">
         <OfertaEstagio :estagio="estagio" @click="this.$router.push(`/aluno/estagio/id/${estagio.id}`)"/>
       </v-col>
     </v-row>
@@ -16,20 +60,39 @@
 import Header from "@/components/Header.vue";
 import ButtonCard from "@/components/ButtonCard.vue";
 import PageTitle from "@/components/PageTitle.vue";
-import {getAllEstagios} from "@/services/Estagio.js";
+import {getAllEstagios, 
+        getEmpresas,
+        getCargos, 
+        getModalidades} from "@/services/Estagio.js";
 import OfertaEstagio from "@/components/ofertaEstagio.vue";
 import Loading from "@/components/Loading.vue";
 import PopUpErro from "@/components/PopUpErro.vue";
 
+import { watch } from 'vue';
+
+
 export default {
   name: "ListEstagioAluno",
   created() {
-    this.getEstagios();
+    this.estagios = this.getEstagios();
+
+    watch(() => this.empresasSelecionadas, this.filterCards);
+    watch(() => this.cargosSelecionados, this.filterCards);
+    watch(() => this.modalidadesSelecionadas, this.filterCards);
   },
   data() {
     return {
       estagios: [],
       erro: false,
+      filtros: false,
+
+      empresas: [],
+      empresasSelecionadas: [],
+      cargos: [],
+      cargosSelecionados:[],
+      modalidades: [],
+      modalidadesSelecionadas: [],
+      estagiosFiltrados: [],
     }
   },
   components: {
@@ -49,11 +112,82 @@ export default {
       catch (e) {
         this.erro = e.response.data.message;
       }
+      this.estagiosFiltrados = this.estagios;
+      this.empresas = getEmpresas(this.estagios);
+      this.cargos = getCargos(this.estagios);
+      this.modalidades = getModalidades(this.estagios);
+    },
+    filterCards() {
+      this.estagiosFiltrados = this.estagios.filter((estagio) => {
+        if (this.empresasSelecionadas.length === 0)
+          return true;
+        return this.empresasSelecionadas.includes(estagio.empresa);
+      });
+
+      this.estagiosFiltrados = this.estagiosFiltrados.filter((estagio) => {
+        if (this.cargosSelecionados.length === 0)
+          return true;
+        return this.cargosSelecionados.includes(estagio.cargo);
+      });
+
+      this.estagiosFiltrados = this.estagiosFiltrados.filter((estagio) => {
+        if (this.modalidadesSelecionadas.length === 0)
+          return true;
+        return this.modalidadesSelecionadas.includes(estagio.modalidade);
+      });
     }
-  }
+  },
 }
 </script>
 
 <style scoped>
+
+#addFiltro {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  
+  align-self: center; 
+
+  color: white;
+  font-weight: bold;
+  background-color: var(--green3);
+  border-radius: 10px;
+}
+
+#closeFiltros {
+  align-self: flex-end;
+}
+
+#closeFiltros:hover {
+  cursor: pointer
+}
+
+.filter-container {
+  display: flex;
+  flex-direction: column;
+  width: 60vw;
+  border-radius: 15px;
+  border-width: 1.5px;
+  border-color: rgba(0, 0, 0, 0.9);
+  border-style: solid;
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+.selects-container {
+  align-self: center;
+  width: 50vw;
+}
+
+@media (max-width: 700px) {
+  .filter-container {
+    width: 95vw;
+    
+  }
+  .selects-container {
+    width: 85 vw;
+  }
+}
 
 </style>
--- src/services/Estagio.js
+++ src/services/Estagio.js
@@ -19,3 +19,36 @@ export const createEstagio = async (params) => {
 
     return data;
 }
+
+export const getEmpresas = (estagios) => {
+    let empresas = [];
+    for(let estagio of estagios) {
+        if (!empresas.includes(estagio.empresa)) {
+            empresas.push(estagio.empresa);
+        }
+    }
+
+    return empresas;
+}
+
+export const getCargos = (estagios) => {
+    let cargos = [];
+    for(let estagio of estagios) {
+        if (!cargos.includes(estagio.cargh)) {
+            cargos.push(estagio.cargo);
+        }
+    }
+
+    return cargos;
+}
+
+export const getModalidades = (estagios) => {
+    let modalidades = [];
+    for(let estagio of estagios) {
+        if (!modalidades.includes(estagio.modalidade)) {
+            modalidades.push(estagio.modalidade);
+        }
+    }
+
+    return modalidades;
+}
