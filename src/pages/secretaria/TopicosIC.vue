e090458a5276a8837b7077dee1a32e6c7a12a553
Tiago d'Almeida <dalmeidatiago@gmail.com>
2024-01-29 10:02:03 -0300

    Modificações esteticas e inicio do front de Editar topico

--- src/pages/secretaria/TopicosIC.vue
+++ src/pages/secretaria/TopicosIC.vue
@@ -8,34 +8,56 @@
       <v-form id="form">
         <div id="text">
           <p>Título</p>
-          <v-text-field id="novoTitulo" variant="outlined"
+          <v-text-field id="novoTitulo" v-model="titulo" variant="outlined"
                         :rules="[v => !!v || 'Este campo é obrigatório']" style="width: 80%"/>
         </div>
 
         <div id="submit">
           <v-btn id="salvar" @click="">Salvar</v-btn>
-          <v-btn id="cancelar" @click="toggle = !toggle">Cancelar</v-btn>
+          <v-btn class="cancelar" @click="resetForm; toggle = !toggle">Cancelar</v-btn>
         </div>
       </v-form>
     </v-card>
   </div>
-</template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="auto">
+        <ButtonCard title="Teste" @click="hidden = !hidden"/>
+        <div id="edit" v-if="hidden">
+          <v-btn id="editar" @click="hidden = !hidden">Editar</v-btn>
+          <v-btn class="cancelar" @click="hidden = !hidden">Cancelar</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container></template>
 
 <script>
 
 import PageTitle from "@/components/PageTitle.vue";
 import ButtonCard from "@/components/ButtonCard.vue";
+import OfertaEstagio from "@/components/ofertaEstagio.vue";
 
 export default {
   data() {
     return {
-      toggle: false
+      titulo: '',
+      toggle: false,
+      hidden: false,
     }
   },
+
   name: "SecretariaTopicos",
+
   components: {
+    OfertaEstagio,
     PageTitle,
     ButtonCard,
+  },
+
+  methods: {
+    resetForm() {
+      this.titulo = '';
+    },
   },
 }
 </script>
@@ -54,7 +76,11 @@ export default {
 #createTopico {
   background-color: var(--green1);
   height: 150px;
-  width: 50vw;
+  width: 25vw;
+}
+
+#text {
+  font-size: 1.2rem;
 }
 
 #submit {
@@ -64,16 +90,29 @@ export default {
 }
 
 #salvar {
-  width: 150px;
+  width: 90px;
   color: white;
   background-color: #27AE60;
   box-shadow: 0 0 2px rgba(0, 0, 0, 0.21);
 }
 
-#cancelar {
-  width: 150px;
+.cancelar {
+  width: 90px;
   color: white;
   background-color: #EB5757;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.21);
+}
+
+#edit {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+#editar {
+  width: 90px;
+  color: white;
+  background-color: #2752ae;
   box-shadow: 0 0 2px rgba(0, 0, 0, 0.21);
 }
 
