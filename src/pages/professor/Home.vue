ae296f57f13d43a3ddd4e0a16fd268ac3ac62cce
Gian <gianpmaule@gmail.com>
2024-02-01 15:02:34 -0300

    [FEAT] Autenticação nas rotas de professor

--- src/pages/professor/Home.vue
+++ src/pages/professor/Home.vue
@@ -1,35 +1,51 @@
 <template>
-    <PageTitle title="PORTAL DO PROFESSOR"/>
-    <DadosPessoais/>
-    <ButtonCard title="Iniciação Científica"  @click="this.$router.push('/professor/ic')"/>
-    <ButtonCard title="Pedidos" @click="this.$router.push('/professor/pedidos')"/>
-  </template>
-  
-  <script>
- 
-  import PageTitle from "@/components/PageTitle.vue"
-  import ButtonCard from "@/components/ButtonCard.vue"
-  import DadosPessoais from "@/components/DadosPessoais.vue";
-  
-  
-  export default {
-    name: "ProfessorInicial",
-    components: {
-      DadosPessoais,
-      PageTitle,
-      ButtonCard,
-    },
-  }
-  
-  </script>
-  
-  <style scoped>
-  
-  .container {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  </style>
+	<PageTitle title="PORTAL DO PROFESSOR" />
+	<DadosPessoais
+		:nome="this.usuario.nome"
+		:matricula="this.usuario.matricula"
+	/>
+	<ButtonCard
+		title="Iniciação Científica"
+		@click="this.$router.push('/professor/ic')"
+	/>
+	<ButtonCard
+		title="Pedidos"
+		@click="this.$router.push('/professor/pedidos')"
+	/>
+</template>
+
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+import ButtonCard from "@/components/ButtonCard.vue";
+import DadosPessoais from "@/components/DadosPessoais.vue";
+import { getUsuario } from "@/services/sessaoService";
+
+export default {
+	data() {
+		return {
+			usuario: {
+				nome: "",
+				matricula: "",
+			},
+		};
+	},
+	async created() {
+		this.usuario = await getUsuario();
+	},
+	name: "ProfessorInicial",
+	components: {
+		DadosPessoais,
+		PageTitle,
+		ButtonCard,
+	},
+};
+</script>
+
+<style scoped>
+.container {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+</style>
