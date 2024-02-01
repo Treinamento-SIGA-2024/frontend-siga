a972c06efb012c7d703d2055d64fd0d8b26eae26
Gian <gianpmaule@gmail.com>
2024-02-01 14:37:06 -0300

    [FEAT] Rota de aluno agora mostra home com dados da sessão

--- src/pages/aluno/Home.vue
+++ src/pages/aluno/Home.vue
@@ -1,36 +1,45 @@
 <template>
-  <PageTitle title="PORTAL DO ALUNO" />
-  <DadosPessoais />
-  <ButtonCard
-    title="Iniciação Científica"
-    @click="this.$router.push('/aluno/ic')"
-  />
-  <ButtonCard 
-    title="Estágio" 
-    @click="this.$router.push('/aluno/estagio')" 
-  />
-  <ButtonCard 
-    title="Pedidos" 
-    @click="this.$router.push('/aluno/pedidos')" 
-  />
+	<PageTitle title="PORTAL DO ALUNO" />
+	<DadosPessoais
+		:nome="this.usuario.nome"
+		:matricula="this.usuario.matricula"
+	/>
+	<ButtonCard
+		title="Iniciação Científica"
+		@click="this.$router.push('/aluno/ic')"
+	/>
+	<ButtonCard title="Estágio" @click="this.$router.push('/aluno/estagio')" />
+	<ButtonCard title="Pedidos" @click="this.$router.push('/aluno/pedidos')" />
 </template>
 
 <script>
-import Header from '@/components/Header.vue';
-import PageTitle from '@/components/PageTitle.vue';
-import ButtonCard from '@/components/ButtonCard.vue';
-import DadosPessoais from '@/components/DadosPessoais.vue';
+import Header from "@/components/Header.vue";
+import PageTitle from "@/components/PageTitle.vue";
+import ButtonCard from "@/components/ButtonCard.vue";
+import DadosPessoais from "@/components/DadosPessoais.vue";
+
+import { getUsuario } from "@/services/sessaoService";
 
 export default {
-  name: 'AlunoInicial',
-  components: {
-    DadosPessoais,
-    Header,
-    PageTitle,
-    ButtonCard,
-  },
+	name: "AlunoInicial",
+	components: {
+		DadosPessoais,
+		Header,
+		PageTitle,
+		ButtonCard,
+	},
+	data() {
+		return {
+			usuario: {
+				nome: "",
+				matricula: "",
+			},
+		};
+	},
+	async created() {
+		this.usuario = await getUsuario();
+	},
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
