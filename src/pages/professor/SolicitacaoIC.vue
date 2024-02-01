ec8a67e5d51f82028108413a7589bbc478d83f7b
vitria de souza serafim <vitoriass@dcc.ufrj.br>
2024-01-31 23:16:53 -0300

    [Feature]Componente de visualizar os requerimentos Foi feito um componente tipo Sanfona para visualizar os requerimentos dos alunos para cada IC que um professor faz parte; [equipesiga/treinamento-dev-2024/boardtreinamentodev#57]

--- src/pages/professor/SolicitacaoIC.vue
+++ src/pages/professor/SolicitacaoIC.vue
@@ -4,79 +4,130 @@
       SOLICITAÇÃO IC
     </v-card-title>
   </v-card>
-  <v-container v-if="this.inscricoes.length">
-    <v-row v-for="(inscricao, i) in inscricoes" :key="i" cols="auto">
-      <v-col>
-        <CardSolicitacaoIC :inscricao="inscricao" :iniciacaoCientifica="iniciacaoCientifica" @refreshSituacao = "getInscricoesIc"/>
-      </v-col>
-    </v-row>
-  </v-container>
 
-  <v-card :flat="true"  class="semDados" v-if="!this.inscricoes.length">
-    <PopUp :acoes="metodos"/>
+  <v-expansion-panels variant="accordion">
+    <v-expansion-panel
+      v-for="(iniciacao, index) in iniciacoesCientificas"
+      :key="index"
+    >
+      <v-expansion-panel-title @click="toggleAccordion(index)" expand-icon="mdi-menu-down" collapse-icon="mdi-menu-up">
+        {{ iniciacao.nome }}
+      </v-expansion-panel-title>
+      <div v-if="activeIndex === index">
+        <v-row v-for="(inscricao, i) in iniciacao.inscricoes" :key="i" cols="auto">
+          <v-col>
+            <CardSolicitacaoIC
+              :inscricao="inscricao"
+              :iniciacaoCientifica="iniciacao"
+              @refreshSituacao=""
+            />
+          </v-col>
+        </v-row>
+      </div>
+    </v-expansion-panel>
+  </v-expansion-panels>
+
+
+  <v-card :flat="true" class="semDados" v-if="!this.iniciacoesCientificas">
+    <PopUp :acoes="metodos" />
+    <Loading/>
   </v-card>
 </template>
 
 <script>
 import DadosPessoais from "@/components/DadosPessoais.vue";
 import CardSolicitacaoIC from "@/components/CardSolicitacaoIC.vue";
-import {getAllIcPendentes} from "@/services/professorService.js";
+import { getAllIcPendentes } from "@/services/professorService.js";
 import Loading from "@/components/Loading.vue";
 import PopUp from "@/components/PopUp.vue";
-import {getIcById} from "@/services/iniciacaoCientifica.js";
+import { getIcById } from "@/services/iniciacaoCientifica.js";
 
 export default {
-  components: {PopUp, Loading, CardSolicitacaoIC, DadosPessoais},
-  data(){
+  components: { PopUp, Loading, CardSolicitacaoIC, DadosPessoais },
+  data() {
     return {
+      activeIndex: null,
       professorMatricula: "200000001",
       icId: "1",
-      inscricoes:[],
-      iniciacaoCientifica:{},
-      metodos:{
-        msg:"Recarregar página?",
-        aceitarAction:this.getInscricoesIc,
-        cancelarAction:''
-      }
-    }
-  },methods:{
+      inscricoes: [],
+      iniciacoesCientificas: [
+        {
+          id: 1,
+          nome: "Primeira IC do mundo",
+          inscricoes: [{ id: 1, aluno: "Vitória", matricula:"2566662" }],
+        },
+        {
+          id: 2,
+          nome: "Projeto de Pesquisa em Biologia",
+          inscricoes: [
+            { id: 2, aluno: "João", matricula:"25695636" },
+            { id: 3, aluno: "Maria", matricula:"25695636" },
+          ],
+        },
+        {
+          id: 3,
+          nome: "Iniciação Científica em Física",
+          inscricoes: [
+            { id: 4, aluno: "Pedro", matricula:"12036589" },
+            { id: 5, aluno: "Ana", matricula:"12036589" },
+            { id: 6, aluno: "Carlos", matricula:"12036589" },
+          ],
+        },
+      ],
+      metodos: {
+        msg: "Recarregar página?",
+        aceitarAction: this.getInscricoesIc,
+        cancelarAction: "",
+      },
+    };
+  },
+  methods: {
     async getInscricoesIc() {
-      console.log("ooooi")
-      const inscricoes = await getAllIcPendentes(this.professorMatricula, this.icId);
+      console.log("ooooi");
+      const inscricoes = await getAllIcPendentes(
+        this.professorMatricula,
+        this.icId
+      );
       const ic = await getIcById(this.icId);
 
-      console.log(inscricoes)
-      console.log(ic)
+      console.log(inscricoes);
+      console.log(ic);
       this.iniciacaoCientifica = ic;
       this.inscricoes = inscricoes;
-      console.log(this.inscricoes.length)
-    }
-  },created() {
+      console.log(this.inscricoes.length);
+    },
+    toggleAccordion(index) {
+      if (this.activeIndex === index) {
+        this.activeIndex = null;
+      } else {
+        this.activeIndex = index;
+      }
+    },
+  },
+  created() {
     this.getInscricoesIc();
-  }
-}
-
-
+  },
+};
 </script>
 
 <style scoped>
-v-card{
+v-card {
   color: #666666;
   font-weight: bold;
 }
 
-.semDados{
+.semDados {
   position: fixed;
   z-index: 1;
   height: 100vh;
   width: 100%;
-  background-color:rgba(0,0,0,0.3);
+  background-color: rgba(0, 0, 0, 0.3);
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.recarregarBtn{
+.recarregarBtn {
   width: 30%;
   background-color: blue;
 }
